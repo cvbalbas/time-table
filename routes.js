333219f0acc5535edfc16c86201fa941193f48b0
Caryl Anne Balbas <caryl.anne.balbas@eee.upd.edu.ph>
2022-05-05 22:26:15 +0100

    functions from server.js put into separate file called services.js

--- routes.js
+++ routes.js
@@ -1,15 +1,16 @@
 module.exports = {router}
+const {updateTimetableTable, getDataFromTimetableTable} = require("./services")
 
-function router(app, fns){
+function router(app){
     app.get('/', function(request, response){
-        fns.getDataFromTimetableTable(function (data){
+        getDataFromTimetableTable(function (data){
             response.render('timetable', {data:data});
         })
     });
     app.post("/updateTimetableTable", function (request, response){
         var student = request.body.student
         var cell = request.body.cell
-        fns.updateTimetableTable(cell, student)
-        fns.getDataFromTimetableTable()
+        updateTimetableTable(cell, student)
+        getDataFromTimetableTable(function (data){})
     })
 }
