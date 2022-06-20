c10668a810d111ddc18853ddc174e0905a940a26
Hisham Mohammed <hish1993@googlemail.com>
2022-06-20 22:31:30 +0100

    added 'add tutor' functionality

--- connect.js
+++ connect.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 
-var con = mysql.createConnection({host: "localhost", user: "root", password: "", database: "timetable" });
+var con = mysql.createConnection({host: "localhost", user: "root", password: "", database: "timetable", multipleStatements: "true" });
 con.connect(function(err) { 
     if (err) throw err;
     console.log("Connected!");
--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 module.exports = router
 const req = require("express/lib/request")
-const {updateTimetableTable, getDataFromTimetableTable, addTimetableTable, refreshTimetableBrowserData} = require("./services")
+const {updateTimetableTable, getDataFromTimetableTable, addTimetableTable, refreshTimetableBrowserData, addTutor, getTutorNames} = require("./services")
 
 function router(app, wss){
     wss.on("connection", function connection(ws){
@@ -12,6 +12,12 @@ function router(app, wss){
     app.get('/', function(request, response){
             response.render('timetable');
     });
+    app.post("/getTutorNames", function (request, response){
+        var tutorNames = getTutorNames(function (data){
+            response.send(data)
+        })
+
+    })
     app.post("/getTimetableData", function (request, response){
         var tutor = request.body.tutor
         getDataFromTimetableTable(tutor, function(data){
@@ -23,6 +29,7 @@ function router(app, wss){
         var student = request.body.student
         var cell = request.body.cell
         var tablename = request.body.tablename
+        console.log(tablename)
         var quantity = request.body.quantity
         updateTimetableTable(tutor, cell, student, tablename, quantity, wss)
     })
@@ -30,5 +37,10 @@ function router(app, wss){
         var tutor = request.body.tutor
         var weekdate = request.body.weekdate
         addTimetableTable(tutor, weekdate)
+    })
+    app.post("/addTutor", function (request, response) {
+        var newTutor = request.body.newTutor
+        addTutor(newTutor)
+        response.send(newTutor+" added")
     })
 }
